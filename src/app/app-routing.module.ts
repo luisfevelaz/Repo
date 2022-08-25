21328b3d6a59818844aafd399281f4bf99b2ba79
luisvelaz <luis.velazquez@linkthinks.com>
2022-08-25 17:59:37 -0500

    Nuevo proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoadComponent } from './load/load.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'load', component: LoadComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
