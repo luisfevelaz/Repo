d061c42bd3cfa21c8c1225383b8da6ff27e84a0c
luisfevelaz <luisfevelaz00@gmail.com>
2022-11-27 22:03:19 -0600

    gitignore

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'repositorio'
+  database : 'repo'
 });
 
 const puerto = 3000;
