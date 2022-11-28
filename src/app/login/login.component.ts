6e99f425b07ff6790ba77f8c8401a9e9f152a3fe
luisfevelaz <luisfevelaz00@gmail.com>
2022-11-27 21:54:08 -0600

    redireccion login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,7 @@ export class LoginComponent implements OnInit {
   
   url='http://192.168.100.6:3000/login'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _router: Router) { }
 
   ngOnInit(): void {
   }
@@ -31,6 +32,11 @@ export class LoginComponent implements OnInit {
 
     this.http.post(this.url,body,{responseType: 'text'}).subscribe((result) => {
       console.log(result);
+      let resp = JSON.parse(result);
+
+      if(resp.response == 200){
+        this._router.navigateByUrl('/home');
+      }
       
     });
     
