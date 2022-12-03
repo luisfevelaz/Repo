769d2a644b17ba126e929a8f368c6c01f8c07575
didihdz <didihdzmoli@gmail.com>
2022-12-03 14:43:59 -0600

    UPDATE navbar login button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,10 +12,13 @@ export class LoginComponent implements OnInit {
 
   login = new FormGroup({
     email: new FormControl('',Validators.email),
-    password: new FormControl('',Validators.minLength(3))
+    password: new FormControl('',Validators.minLength(3)),
+    nombre: new FormControl('',Validators.maxLength(30))
+    //falta validator de nombre
   });
   
   url='http://192.168.100.80:3000/login'
+  // url='http://192.168.100.80:3000/login'
 
   constructor(private http: HttpClient, private _router: Router) { }
 
@@ -27,7 +30,8 @@ export class LoginComponent implements OnInit {
     
     let body: any = {
       username: this.login.get("email").value,
-      password: this.login.get("password").value
+      password: this.login.get("password").value,
+      nombre: this.login.get("nombre").value
     };
 
     this.http.post(this.url,body,{responseType: 'text'}).subscribe((result) => {
@@ -38,6 +42,7 @@ export class LoginComponent implements OnInit {
         localStorage.clear();
         localStorage.setItem('id',resp.idUser);
         localStorage.setItem('user',resp.username);
+        localStorage.setItem('nombre',resp.nombre);
         this._router.navigate(['/home'],resp.idUser);
       }
       
